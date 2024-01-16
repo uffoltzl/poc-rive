d08123694158e829dc412e13764b56715ccd02e5
uffoltzl <l.ufz@hotmail.fr>
2024-01-16 11:46:38 +0100

    remove resource modification expo-fonts does not do it

--- plugins/withRiveAssets.js
+++ plugins/withRiveAssets.js
@@ -49,43 +49,6 @@ function addAndroidResources(config) {
         fs.copyFileSync(srcAssetPath, destAssetPath);
       }
 
-      // Update the Android resources XML file
-      const resourcesXmlPath = path.join(resDir, "values", "resources.xml");
-
-      let resourcesXml;
-      if (fs.existsSync(resourcesXmlPath)) {
-        resourcesXml = await fs.readFile(resourcesXmlPath, "utf-8");
-      } else {
-        resourcesXml = "<resources>\n</resources>";
-      }
-
-      const rawResourcesTags = assetFiles.map(
-        (assetFile) =>
-          `<string name="${assetFile.substring(
-            0,
-            assetFile.lastIndexOf(".")
-          )}">${assetFile}</string>`
-      );
-
-      const rawResourcesRegex = /<string name="[^"]+">[^<]+<\/string>/;
-
-      for (const rawResourcesTag of rawResourcesTags) {
-        if (resourcesXml.match(rawResourcesRegex)) {
-          resourcesXml = resourcesXml.replace(
-            rawResourcesRegex,
-            rawResourcesTag
-          );
-        } else {
-          const stringResourcesRegex = /<\/resources>/;
-          resourcesXml = resourcesXml.replace(
-            stringResourcesRegex,
-            `${rawResourcesTag}\n    </resources>`
-          );
-        }
-      }
-
-      await fs.writeFile(resourcesXmlPath, resourcesXml);
-
       return config;
     },
   ]);
